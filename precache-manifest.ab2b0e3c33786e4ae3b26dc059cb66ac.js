484a5db803d58d3a7f5cbb14baeb226e8d575243
gbvivian <gbvivian@hotmail.com>
2019-01-20 20:09:03 -0800

    Updates

---
+++ precache-manifest.ab2b0e3c33786e4ae3b26dc059cb66ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5599aa032baebf24fc3f",
+    "url": "/Shopify-Web-Challenge-2019/static/js/runtime~main.5599aa03.js"
+  },
+  {
+    "revision": "05f6bbcab17c7af1ee13",
+    "url": "/Shopify-Web-Challenge-2019/static/js/main.05f6bbca.chunk.js"
+  },
+  {
+    "revision": "93e5498c018d9ab07733",
+    "url": "/Shopify-Web-Challenge-2019/static/js/1.93e5498c.chunk.js"
+  },
+  {
+    "revision": "05f6bbcab17c7af1ee13",
+    "url": "/Shopify-Web-Challenge-2019/static/css/main.6b9ab108.chunk.css"
+  },
+  {
+    "revision": "4f854451150d296255c7a2d0f7135f48",
+    "url": "/Shopify-Web-Challenge-2019/index.html"
+  }
+];
